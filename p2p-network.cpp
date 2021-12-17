3b6c65aa7e8c2619c00bc8747a47490e8c2aef65
Yurok <predbannikov.yurii@gmail.com>
2021-12-17 13:49:21 -0500

    fix

--- p2p-network.cpp
+++ p2p-network.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
+#include <rapidjson/document.h>
 
+using namespace rapidjson;
 using namespace boost::asio;
 
 typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
 
 void client_session(socket_ptr sock);
 
+std::string* loadStringJson(const std::string path) {
+        std::ifstream file(path);
+        if (!file.is_open()) {
+                std::cerr << "could not open file " << path << std::endl;
+                exit(EXIT_FAILURE);
+        }
+        std::string* ret = new std::string(std::istream_iterator<char>(file), std::istream_iterator<char>());
+        file.close();
+        return ret;
+}
+
 int main(int argc, char *argv[]) {
+
     std::cout << "Path: " << argv[0] << std::endl;
     boost::asio::io_service service;
     boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), 2001);
@@ -20,7 +36,6 @@ int main(int argc, char *argv[]) {
         boost::thread( boost::bind(client_session, sock));
         std::cout << "new connection: " << std::endl;
     }
-
 
     return 0;
 }
@@ -36,7 +51,11 @@ void client_session(socket_ptr sock)
             return ; // Connection refused
         if (len > 0)
             write(*sock, boost::asio::buffer("ok", 2));
+        boost::asio::ip::tcp::endpoint ep = sock->remote_endpoint();
+        std::cout << "address " << ep.address().to_string() << std::endl;
+        std::cout << "port " << ep.port() << std::endl;
         std::cout << "iteration ended" << std::endl;
+
     }
 }
 
