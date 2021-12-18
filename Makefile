3ce35b98fde549550d87835bf72ac6b67ecab8e1
Yurok <predbannikov.yurii@gmail.com>
2021-12-18 15:04:04 +0300

    added funcs get_string_myip load_json save_json

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-CXXFLAGS=-g -O0 -std=c++17
+BOOST_ROOT = /usr/include/boost_1_78_0
+BOOST_INC = ${BOOST_ROOT}/include
+CXXFLAGS = -g -O0 -std=c++17
 all_files = p2p-network.o
 all_arguments = -pthread -lboost_system -lboost_thread
 TARGET = p2p-network
@@ -7,8 +9,8 @@ CC = g++
 all: $(all_files)
 	$(CC) -o $(TARGET) $(all_files) $(all_arguments)  
 
-main.o: main.cpp 
-	$(CC) -c p2p-network.cpp 
+p2p-network.o: p2p-network.cpp
+	$(CC) -g -O0 -c p2p-network.cpp -I $(BOOST_ROOT) 
 
 clean:
 	rm $(TARGET) $(all_files)
--- p2p-network.cpp
+++ p2p-network.cpp
@@ -1,41 +1,81 @@
+#include <boost/json/error.hpp>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
-#include <rapidjson/document.h>
+#include <boost/json/src.hpp>
 
-using namespace rapidjson;
 using namespace boost::asio;
+
+#define PATH_JSON   "map_address"
+#define SIGNAL_SERVER   "45.128.207.31"
+#define PORT_LISTEN     "2021"
 
 typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
 
 void client_session(socket_ptr sock);
 
-std::string* loadStringJson(const std::string path) {
-        std::ifstream file(path);
+std::string get_string_myip() {
+    boost::asio::io_service service;
+    boost::asio::ip::udp::resolver resolver(service);
+    boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), "google.com", "");
+    boost::asio::ip::udp::resolver::iterator iter = resolver.resolve(query);
+    boost::asio::ip::udp::endpoint eptest = *iter;
+    boost::asio::ip::udp::socket sock(service);
+    sock.connect(eptest);
+    std::string string_ip = sock.local_endpoint().address().to_string();
+    std::cout << string_ip << std::endl;
+    return string_ip;
+}
+
+
+boost::json::value load_json() {
+        std::ifstream file(PATH_JSON);
         if (!file.is_open()) {
-                std::cerr << "could not open file " << path << std::endl;
+                std::cerr << "could not open file to read " << PATH_JSON << std::endl;
                 exit(EXIT_FAILURE);
         }
-        std::string* ret = new std::string(std::istream_iterator<char>(file), std::istream_iterator<char>());
+        std::string* pstr = new std::string(std::istream_iterator<char>(file), std::istream_iterator<char>());
+        boost::json::value value = boost::json::parse(*pstr);
         file.close();
-        return ret;
+        return value;
 }
+
+void save_json(boost::json::object &jobj) {
+    std::ofstream file(PATH_JSON);
+    if(!file.is_open()) {
+        std::cerr << "could not open file to write " << PATH_JSON << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::string str_buf = boost::json::serialize(jobj);
+    file.write(str_buf.c_str(), str_buf.length());
+    file.close();
+}
+
+
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "Path: " << argv[0] << std::endl;
-    boost::asio::io_service service;
-    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), 2001);
-    boost::asio::ip::tcp::acceptor acc(service, ep);
-    while(true) {
-        socket_ptr sock(new boost::asio::ip::tcp::socket(service));
-        acc.accept(*sock);
-        boost::thread( boost::bind(client_session, sock));
-        std::cout << "new connection: " << std::endl;
-    }
+    std::string my_ip = get_string_myip();
+
+    if(my_ip == SIGNAL_SERVER) {
+
+        std::cout << "Path: " << argv[0] << std::endl;
+        boost::asio::io_service service;
+        boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), 2001);
+        boost::asio::ip::tcp::acceptor acc(service, ep);
+        while(true) {
+            socket_ptr sock(new boost::asio::ip::tcp::socket(service));
+            acc.accept(*sock);
+            boost::thread( boost::bind(client_session, sock));
+            std::cout << "new connection: " << std::endl;
+        }
+
+    } else {
+
+    }    
 
     return 0;
 }
